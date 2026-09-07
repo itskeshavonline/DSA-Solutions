315267aad62978dbd64eb549f76537a5e819b661
Keshav <itskeshavonline@gmail.com>
2026-09-08 03:44:00 +0530

    Include C++ offset method for counting bits
    Added C++ offset approach as a comment for counting bits.

--- LeetCode/Easy/Counting Bits/solution.py
+++ LeetCode/Easy/Counting Bits/solution.py
@@ -6,5 +6,31 @@ class Solution:
             # formula build by observing the patterns
             # ans[i] is the bitcount of (i // 2) + 1 (if i is odd)
             ans[i] = ans[i >> 1] + (i & 1)
+            # or
+            # ans[i] = ans[i / 2] + (i % 2)
             
         return ans
+
+# Using Offset approach in C++
+# class Solution {
+# public:
+#     vector<int> countBits(int n) {
+
+#         vector<int> ans(n + 1, 0);
+
+#         int offset = 1;
+
+#         for (int i = 1; i <= n; i++) {
+
+#             // When i reaches the next power of 2
+#             if (i == offset * 2) {
+#                 offset = i;
+#             }
+
+#             // offset has one '1'; remaining part is i - offset
+#             ans[i] = 1 + ans[i - offset];
+#         }
+
+#         return ans;
+#     }
+# };
