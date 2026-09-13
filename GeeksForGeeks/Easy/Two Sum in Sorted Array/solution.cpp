9dabfbf5f893647ab7255f001a60609ba660cf2a
Keshav <itskeshavonline@gmail.com>
2026-09-13 15:34:23 +0530

    [Syncode] GeeksForGeeks/Easy/Two Sum in Sorted Array

---
+++ GeeksForGeeks/Easy/Two Sum in Sorted Array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size()-1;
+        
+        while (low < high) {
+            int sum = arr[low] + arr[high];
+            if (sum == target) {
+                return {low+1, high+1};
+            } else if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        
+        return {-1, -1};
+        
+    }
+};
