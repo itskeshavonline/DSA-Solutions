4289046ec9b48a6d8536db090d46795ae3a05c7a
Keshav <itskeshavonline@gmail.com>
2026-09-17 11:02:43 +0530

    Update solution.cpp

--- LeetCode/Medium/House Robber/solution.cpp
+++ LeetCode/Medium/House Robber/solution.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-int theft(vector<int>& nums, int i, int n, vector<int>& dp) {
-    if (i >= n) {
-        return 0;
-    }
-    if (dp[i] != -1) {
-        return dp[i];
-    }
-
-    int include = nums[i] + theft(nums, i+2, n, dp);
-    int exclude = 0 + theft(nums, i+1, n, dp);
-    dp[i] = max(include, exclude);
-    return dp[i];
-
-}
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        vector<int> dp(n+1, -1);
-        return theft(nums, i, n, dp);
+
+        vector<int> dp(n + 2, 0);
+        dp[n] = 0;     
+        dp[n + 1] = 0; 
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            int include = nums[i] + dp[i + 2];
+            int exclude = dp[i + 1];
+            
+            dp[i] = max(include, exclude);
+        }
+
+        return dp[0];
     }
 };
+
+
+/*
+class Solution {
+public:
+    int solve(vector<int>& nums, vector<int>& dp, int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int include = nums[n] + solve(nums, dp, n - 2);
+        int exclude = 0 + solve(nums, dp, n - 1);
+
+        dp[n] = max(include, exclude);
+        return dp[n];
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        
+        return solve(nums, dp, n-1);
+    }
+};
+*/
